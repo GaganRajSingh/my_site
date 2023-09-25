4ba1806e1aed60ebe1d01ba9626be1722d319155
Gagan Raj Singh <gaganrajsinghbedi@gmail.com>
2023-09-25 13:06:58 -0600

    Updated the project carousel to autoplay

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,9 +9,12 @@ function Projects () {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        // speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true
     };
     return (
         <div className='projects'>
