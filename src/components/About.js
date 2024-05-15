23ac203e16a260289fc2ea63e090761e56cb35e3
Gagan Raj Singh <gaganrajsinghbedi@gmail.com>
2024-05-14 18:54:32 -0700

    Updated about layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,35 +1,76 @@
-import '../css/About.css'
+import "../css/About.css";
 import { MdEmail } from "react-icons/md";
 import { BsTwitter, BsLinkedin, BsGithub } from "react-icons/bs";
 import { SiLeetcode } from "react-icons/si";
 
-
-function About () {
-    const image_src = require("../assets/photo.jpeg");
-    return (
-        <div className='about'>
-            <div className='column'>
-                <div className="avatar">
-                    <img src={image_src} className="avatar_image" alt="Avatar" />
-                </div>
-                <div className='socials'>
-                    <ul>
-                        <li><a target="_blank" href='https://github.com/gaganrajsingh'> <BsGithub />   </a></li>
-                        <li><a target="_blank" href="https://linkedin.com/in/gagan-raj-singh"> <BsLinkedin /> </a></li>
-                        <li><a target="_blank" href="https://leetcode.com/grsb"> <SiLeetcode /> </a></li>
-                        <li><a target="_blank" href="https://twitter.com/Gagan_2610"> <BsTwitter />  </a></li>
-                        <li><a target="_blank" href="mailto:grsingh@ualberta.ca"> <MdEmail size={21}/>    </a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className='column'>
-                <div className='intro'>
-                    <h1>Hi there 👋🏻</h1>
-                    <p>I'm Gagan Raj Singh, a software developer and a computer science graduate student at University of Alberta.</p>
-                </div>
-            </div>
-        </div>
-    );
+function About() {
+	const image_src = require("../assets/photo.jpeg");
+	return (
+		<div className="about">
+			<div className="column">
+				<div className="avatar">
+					<img
+						src={image_src}
+						className="avatar_image"
+						alt="Avatar"
+					/>
+				</div>
+				<div className="socials">
+					<ul>
+						<li>
+							<a
+								target="_blank"
+								href="https://github.com/gaganrajsingh"
+							>
+								{" "}
+								<BsGithub />{" "}
+							</a>
+						</li>
+						<li>
+							<a
+								target="_blank"
+								href="https://linkedin.com/in/gagan-raj-singh"
+							>
+								{" "}
+								<BsLinkedin />{" "}
+							</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://leetcode.com/grsb">
+								{" "}
+								<SiLeetcode />{" "}
+							</a>
+						</li>
+						<li>
+							<a
+								target="_blank"
+								href="https://twitter.com/Gagan_2610"
+							>
+								{" "}
+								<BsTwitter />{" "}
+							</a>
+						</li>
+						<li>
+							<a
+								target="_blank"
+								href="mailto:grsingh@ualberta.ca"
+							>
+								{" "}
+								<MdEmail size={21} />{" "}
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="column">
+				<div className="intro">
+					<p>👋🏻 Hi, I'm</p>
+					<h1>Gagan Raj Singh</h1>
+					<p>Software developer and a CS grad student @UofA.</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default About
+export default About;
